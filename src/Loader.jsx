a5f434f5f133d7fca76015c513e90a17b152a5c9
boytemirxayitmurodov@gmail.com <boytemirxayitmurodov@gmail.com>
2022-09-14 15:01:49 +0500

    React js da Tabla

---
+++ src/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './loader.css';
+
+const Loader = () => {
+  return (
+     <div className="container">
+         <span className="loader-2">Loading</span>
+          <span className="loader"></span>
+     </div>
+  )
+}
+
+export default Loader
